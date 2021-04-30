bf5cbfff61235d87bde73317a39015dffdee2e0d
Kenny Pflug <kenny.pflug@live.de>
2021-04-30 09:35:52 +0200

    added AsyncReadOnlySession

---
+++ Code/Synnotech.Linq2Db.MsSqlServer.Tests/AsyncReadOnlySessionTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Synnotech.Linq2Db.MsSqlServer.Tests
+{
+    public static class AsyncReadOnlySessionTests
+    {
+        [Fact]
+        public static void MustImplementIDisposable() =>
+            typeof(AsyncReadOnlySession<>).Should().Implement<IDisposable>();
+
+        [Fact]
+        public static void MustImplementIAsyncDisposable() =>
+            typeof(AsyncReadOnlySession<>).Should().Implement<IAsyncDisposable>();
+    }
+}
