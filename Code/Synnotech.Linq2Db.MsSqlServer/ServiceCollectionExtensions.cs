c56728de30df5a5d9a29bb05aadc2420ad00d722
Kenny Pflug <kenny.pflug@live.de>
2021-05-03 13:54:58 +0200

    added an extension method for registering an async session that automatically creates a transaction for the underlying data connection

--- Code/Synnotech.Linq2Db.MsSqlServer/ServiceCollectionExtensions.cs
+++ Code/Synnotech.Linq2Db.MsSqlServer/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Light.GuardClauses;
 using LinqToDB.Configuration;
 using LinqToDB.Data;
@@ -91,8 +92,8 @@ namespace Synnotech.Linq2Db.MsSqlServer
 
             var logger = loggerFactory.CreateLogger<DataConnection>();
             return optionsBuilder.WithTraceLevel(traceLevel)
-                          .WriteTraceWith(logger.LogLinq2DbMessage)
-                          .Build();
+                                 .WriteTraceWith(logger.LogLinq2DbMessage)
+                                 .Build();
         }
 
         /// <summary>
@@ -125,5 +126,87 @@ namespace Synnotech.Linq2Db.MsSqlServer
                     throw new ArgumentOutOfRangeException(nameof(traceLevel), traceLevel, $"The trace level \"{traceLevel}\" is unknown.");
             }
         }
+
+        /// <summary>
+        /// Registers the specified session as a <see cref="Task{TAbstraction}" /> with a transient lifetime. When this dependency is resolved,
+        /// the session and a data connection will be instantiated, a transaction will be started asynchronously, and finally the data connection
+        /// is injected into the session. In your client code, you should request a Func&lt;Task&lt;TAbstraction>> so that you can instantiate, use, and
+        /// afterwards dispose a session:
+        /// <code>
+        /// public class MySessionClient
+        /// {
+        ///     public MySessionClient(Func&lt;Task&lt;IMySession>> createSessionAsync) =>
+        ///         CreateSessionAsync = createSessionAsync;
+        ///
+        ///     private Func&lt;Task&lt;IMySession>> CreateSessionAsync { get; }
+        ///
+        ///     public async Task SomeMethod()
+        ///     {
+        ///         await using var session = await CreateSessionAsync();
+        ///         // do something useful with your session
+        ///     }
+        /// }
+        /// </code>
+        /// </summary>
+        /// <typeparam name="TAbstraction">The interface that your session implements.</typeparam>
+        /// <typeparam name="TImplementation">The Linq2Db session implementation that performs the actual database I/O.</typeparam>
+        /// <typeparam name="TDataConnection">Your custom data connection subtype that you use in your solution.</typeparam>
+        /// <param name="services">The collection that holds all registrations for the DI container.</param>
+        /// <param name="registerFactoryDelegate">
+        /// The value indicating whether a factory delegate should be registered as a singleton. The default value is true. You should
+        /// set this to false if your DI container can support function factories out of the box, like e.g. LightInject (https://www.lightinject.net/#function-factories).
+        /// </param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is null.</exception>
+        public static IServiceCollection AddAsyncSession<TAbstraction, TImplementation, TDataConnection>(this IServiceCollection services, bool registerFactoryDelegate = true)
+            where TImplementation : AsyncSession, TAbstraction, new()
+            where TDataConnection : DataConnection
+        {
+            services.MustNotBeNull(nameof(services));
+            services.AddTransient<Task<TAbstraction>>(async container =>
+            {
+                var dataConnection = container.GetRequiredService<TDataConnection>();
+                var session = new TImplementation();
+                await dataConnection.BeginTransactionAsync(session.TransactionLevel);
+                session.SetDataConnection(dataConnection);
+                return session;
+            });
+            if (registerFactoryDelegate)
+                services.AddSingleton<Func<Task<TAbstraction>>>(container => container.GetRequiredService<Task<TAbstraction>>);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Registers the specified session as a <see cref="Task{TAbstraction}" /> with a transient lifetime. When this dependency is resolved,
+        /// the session and a data connection will be instantiated, a transaction will be started asynchronously, and finally the data connection
+        /// is injected into the session. In your client code, you should request a Func&lt;Task&lt;TAbstraction>> so that you can instantiate, use, and
+        /// afterwards dispose a session:
+        /// <code>
+        /// public class MySessionClient
+        /// {
+        ///     public MySessionClient(Func&lt;Task&lt;IMySession>> createSessionAsync) =>
+        ///         CreateSessionAsync = createSessionAsync;
+        ///
+        ///     private Func&lt;Task&lt;IMySession>> CreateSessionAsync { get; }
+        ///
+        ///     public async Task SomeMethod()
+        ///     {
+        ///         await using var session = await CreateSessionAsync();
+        ///         // do something useful with your session
+        ///     }
+        /// }
+        /// </code>
+        /// </summary>
+        /// <typeparam name="TAbstraction">The interface that your session implements.</typeparam>
+        /// <typeparam name="TImplementation">The Linq2Db session implementation that performs the actual database I/O.</typeparam>
+        /// <param name="services">The collection that holds all registrations for the DI container.</param>
+        /// <param name="registerFactoryDelegate">
+        /// The value indicating whether a factory delegate should be registered as a singleton. The default value is true. You should
+        /// set this to false if your DI container can support function factories out of the box, like e.g. LightInject (https://www.lightinject.net/#function-factories).
+        /// </param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is null.</exception>
+        public static IServiceCollection AddAsyncSession<TAbstraction, TImplementation>(this IServiceCollection services, bool registerFactoryDelegate = true)
+            where TImplementation : AsyncSession, TAbstraction, new() =>
+            services.AddAsyncSession<TAbstraction, TImplementation, DataConnection>(registerFactoryDelegate);
     }
 }
