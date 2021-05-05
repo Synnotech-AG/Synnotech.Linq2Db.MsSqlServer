ab5bd25ccec4bd8bcb9a21fa1358d549cab8f3a4
Kenny Pflug <kenny.pflug@live.de>
2021-05-05 07:27:21 +0200

    added base class for integration tests

---
+++ Code/Synnotech.Linq2Db.MsSqlServer.Tests/BaseMsSqlIntegrationTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Light.GuardClauses;
+using Light.GuardClauses.Exceptions;
+using Microsoft.Extensions.Configuration;
+using Synnotech.MsSqlServer;
+using Synnotech.Xunit;
+using Xunit;
+
+namespace Synnotech.Linq2Db.MsSqlServer.Tests
+{
+    public abstract class BaseMsSqlIntegrationTest : IAsyncLifetime
+    {
+        private bool AreIntegrationTestsEnabled => TestSettings.Configuration.GetValue<bool>(nameof(AreIntegrationTestsEnabled));
+
+        private string ConnectionString
+        {
+            get
+            {
+                var connectionString = TestSettings.Configuration["connectionString"];
+                if (connectionString.IsNullOrWhiteSpace())
+                    throw new InvalidConfigurationException("You must set \"connectionString\" when \"areIntegrationTestsEnabled\" is set to true in testsettings.");
+                return connectionString;
+            }
+        }
+
+        public async Task InitializeAsync()
+        {
+            if (!AreIntegrationTestsEnabled)
+                return;
+
+            await Database.DropAndCreateDatabaseAsync(ConnectionString);
+        }
+
+        public Task DisposeAsync() => Task.CompletedTask;
+
+        protected void SkipTestIfNecessary() => Skip.IfNot(AreIntegrationTestsEnabled);
+    }
+}
---
+++ Code/Synnotech.Linq2Db.MsSqlServer.Tests/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using LinqToDB.Mapping;
+
+namespace Synnotech.Linq2Db.MsSqlServer.Tests
+{
+    public static class DatabaseContext
+    {
+        public static void CreateMappings(MappingSchema mappingSchema)
+        {
+            var builder = mappingSchema.GetFluentMappingBuilder();
+
+            builder.Entity<Employee>()
+                   .Property(e => e.Id).IsIdentity().IsPrimaryKey()
+                   .Property(e => e.Name).HasLength(50).IsNullable(false)
+                   .Property(e => e.Age).IsNullable(false);
+        }
+    }
+}
