d93b709ddfe169b6b403c0609eef526bf825ea1a
Kenny Pflug <kenny.pflug@live.de>
2021-05-27 08:19:42 +0200

    AsyncReadOnlySession<T> now implements IAsyncReadOnlySession

--- Code/Synnotech.Linq2Db.MsSqlServer/AsyncReadOnlySession.cs
+++ Code/Synnotech.Linq2Db.MsSqlServer/AsyncReadOnlySession.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Light.GuardClauses;
 using LinqToDB.Data;
+using Synnotech.DatabaseAbstractions;
 
 namespace Synnotech.Linq2Db.MsSqlServer
 {
@@ -10,17 +11,17 @@ namespace Synnotech.Linq2Db.MsSqlServer
     /// is only used to read data (i.e. no data is inserted or updated), thus SaveChangesAsync
     /// is not available. No transaction is needed while this session is active.
     /// Beware: you must not derive from this class and introduce other references to disposable objects.
-    /// Only the <see cref="DataConnection"/> will be disposed.
+    /// Only the <see cref="DataConnection" /> will be disposed.
     /// </summary>
-    /// <typeparam name="TDataConnection">Your database context type that derives from <see cref="DataConnection"/>.</typeparam>
-    public abstract class AsyncReadOnlySession<TDataConnection> : IDisposable, IAsyncDisposable
+    /// <typeparam name="TDataConnection">Your database context type that derives from <see cref="DataConnection" />.</typeparam>
+    public abstract class AsyncReadOnlySession<TDataConnection> : IAsyncReadOnlySession
         where TDataConnection : DataConnection
     {
         /// <summary>
         /// Initializes a new instance of <see cref="AsyncReadOnlySession{TDataConnection}" />.
         /// </summary>
         /// <param name="dataConnection">The Linq2Db data connection used for database access.</param>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="dataConnection"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="dataConnection" /> is null.</exception>
         protected AsyncReadOnlySession(TDataConnection dataConnection) =>
             DataConnection = dataConnection.MustNotBeNull(nameof(dataConnection));
 
@@ -45,7 +46,7 @@ namespace Synnotech.Linq2Db.MsSqlServer
     /// is only used to read data (i.e. no data is inserted or updated), thus SaveChangesAsync
     /// is not available. No transaction is needed while this session is active.
     /// Beware: you must not derive from this class and introduce other references to disposable objects.
-    /// Only DataConnection will be disposed.
+    /// Only the DataConnection will be disposed.
     /// </summary>
     public abstract class AsyncReadOnlySession : AsyncReadOnlySession<DataConnection>
     {
@@ -53,7 +54,7 @@ namespace Synnotech.Linq2Db.MsSqlServer
         /// Initializes a new instance of <see cref="AsyncReadOnlySession" />.
         /// </summary>
         /// <param name="dataConnection">The Linq2Db data connection used for database access.</param>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="dataConnection"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="dataConnection" /> is null.</exception>
         protected AsyncReadOnlySession(DataConnection dataConnection) : base(dataConnection) { }
     }
 }
